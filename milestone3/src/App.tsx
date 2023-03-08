7772884b308a7765d8f3fa5e8bfcb96e5fc0bb02
lysac <lysackm5@gmail.com>
2023-03-07 18:15:26 -0600

    mark initial commit

---
+++ milestone3/src/App.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import logo from './assets/logo.svg';
+import './App.css';
+import { MyFirstComponent } from './components/MyFirstComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        {/* this is how you add your component to other things */}
+        <MyFirstComponent showButton={true}></MyFirstComponent>
+      </header>
+    </div>
+  );
+}
+
+export default App;
